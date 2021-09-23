5b62de15a0ecfb8bd47777b941bccb546ae81c92
Pavan Amin <pavanamin@gmail.com>
2021-09-22 18:59:34 -0700

    added link

--- request.sh
+++ request.sh
@@ -14,6 +14,9 @@ result="$(curl -X POST -H "Authorization: Bearer $(cat access_key.txt)" -H "Cont
 
 rating="$(jq '.data.business.rating' <<< $result)"
 business="$(jq -r '.data.business.name' <<< $result)"
+link="$(jq -r '.data.business.url' <<< $result)"
 
-echo $business has a rating of $rating on Yelp
+
+echo $business has a rating of $rating on Yelp. Click on the link below to find more information on $business:
+echo $link
 
