090e0cb9b87cdd7e79d58ac139c2553d8c239a56
Pavan Amin <pavanamin@gmail.com>
2021-09-23 17:49:47 -0700

    set -x

--- request.sh
+++ request.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -e
+#set -x
 
 result="$(curl -X POST -H "Authorization: Bearer $(cat access_key.txt)" -H "Content-Type: application/graphql" https://api.yelp.com/v3/graphql --data '{
     business(id: "garaje-san-francisco") {
